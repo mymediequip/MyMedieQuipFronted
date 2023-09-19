c2bb7c2c07e41e40f81599aab626be41e8c91c9f
amit <amit.y@gypr.in>
2023-09-19 16:38:51 +0530

    my ads edit page update

--- src/services/index.js
+++ src/services/index.js
@@ -13,7 +13,7 @@ const postData = async (url,formData,isAuthenticated) => {
     headers.Authorization = `Token ${token}`;
   }
   const response = await fetch(`${BaseURL}/${url}`, {
-    mode: "cors",
+    mode: "no-cors",
     method: "POST",
     headers: headers,
     body: formData,
@@ -36,7 +36,7 @@ const postData1 = async (url,body,isAuthenticated) => {
     headers.Authorization = `Token ${token}`;
   }
   const response = await fetch(`${BaseURL}/${url}`, {
-    mode: "cors",
+    mode: "no-cors",
     method: "POST",
     headers: headers,
     body: JSON.stringify(body),
